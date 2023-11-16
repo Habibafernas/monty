ce93a8eb8041cf4ed9ead655f7c9cb55189de12f
Habibafernas <habibafernas@gmail.com>
2023-11-16 12:41:43 -0800

    a

---
+++ push.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * push - function that adds node to the stack
+ * @head: double head pointer to the stack
+ * @count: line count
+ *
+ */
+void push(stack_t **head, unsigned int count)
+{
+	int i, m = 0, f = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			m++;
+		for (; bus.arg[m] != '\0'; m++)
+		{
+			if (bus.arg[m] > 57 || bus.arg[m] < 48)
+				flag = 1;
+		}
+		if (f == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", c);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+	       	}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", c);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	i = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, i);
+	else
+		addqueue(head, i);
+}
